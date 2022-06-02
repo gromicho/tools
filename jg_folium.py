b532482b4b127ae5b23aa61927094421a374de98
Joaquim Gromicho <48596879+gromicho@users.noreply.github.com>
2022-06-02 14:43:58 +0200

    Update jg_folium.py

--- jg_folium.py
+++ jg_folium.py
@@ -1,8 +1,9 @@
+import folium
+
 def GetPoints( data ):
     return data[['FINISH_LATITUDE','FINISH_LONGITUDE']].dropna().drop_duplicates().copy().rename(columns={'FINISH_LATITUDE':'lat','FINISH_LONGITUDE':'lon'})
 
 def MapForPoints( points, zoom_start=8 ):
-    import folium
     a,b=points.min()
     c,d=points.max()
     start_coords = ((c-a)/2,(d-b)/2)
@@ -17,6 +18,15 @@ def MarkPointsOnMap( points, marker=default_marker, describe=default_describer, 
     folium_map = MapForPoints( points )
     for lat,lon in points.values:
         marker(lat,lon,popup=describe(lat,lon),**kwargs).add_to(folium_map)
+    return folium_map
+
+def MarkRouteThroughPointsOnMap( points, folium_map=None, describe=default_describer, **kwargs ):
+    if not folium_map:
+        folium_map = MapForPoints( points )
+    stops = [ (lat,lon) for lat,lon in points.drop_duplicates().dropna().values ]
+    for i,(lat,lon) in enumerate(stops):  
+        folium.Marker((lat,lon), icon=folium.plugins.BeautifyIcon(number=i,**kwargs),popup=describe(lat,lon)).add_to(folium_map)
+    folium.PolyLine(stops, **kwargs).add_to(folium_map)
     return folium_map
 
 def FoliumToPng( folium_map, file_name, rendering_seconds=5 ):
