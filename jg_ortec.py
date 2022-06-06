f01606f6c3702edaeb6b253ca439be1955346ce0
Joaquim Gromicho <48596879+gromicho@users.noreply.github.com>
2022-06-06 19:10:43 +0200

    Create jg_ortec.py

---
+++ jg_ortec.py
@@ -0,0 +1,11 @@
+def ExtractInstance( data, shifts, addresses, D, T, nof_decimals=2 ):
+    import numpy as np
+    these = data.SHIFT.isin( jg.make_iterable(shifts) )
+    aux = data[ these ][['FINISH_LATITUDE','FINISH_LONGITUDE']].dropna().drop_duplicates().reset_index(drop=True)
+    aux['idx'] = [ addresses.loc[ (lat,lon) ].idx for (lat,lon) in aux.values ]
+    orders = data[these & ((data.ACTION =='pickup') | (data.ACTION =='deliver')) ].fillna(0).copy().reset_index(drop=True)
+    orders['ORDER'] = orders.ORDER.astype(int)
+    orders['LM'] = np.round( np.diff([0]+orders.LOAD_LM.to_list()).tolist(), nof_decimals )
+    orders['KG'] = np.round( np.diff([0]+orders.LOAD_KG.to_list()).tolist(), nof_decimals )
+    orders['idx'] = [ addresses.loc[ (lat,lon) ].idx for (lat,lon) in orders[['FINISH_LATITUDE','FINISH_LONGITUDE']].values ]
+    return orders[['ACTION','ORDER','FINISH_LATITUDE','FINISH_LONGITUDE','idx','EARLIEST_START_TIME','LATEST_START_TIME','LM','KG']], aux, D.loc[aux.idx,aux.idx], T.loc[aux.idx,aux.idx]
