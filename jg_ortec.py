5f2432794139fa8c20004ca543ed400b5cc1ef2d
Joaquim Gromicho <48596879+gromicho@users.noreply.github.com>
2022-06-08 16:23:22 +0200

    Update jg_ortec.py

--- jg_ortec.py
+++ jg_ortec.py
@@ -3,7 +3,9 @@ def ExtractInstance( data, shifts, addresses, D, T, nof_decimals=2 ):
     these = data.SHIFT.isin( jg.make_iterable(shifts) )
     aux = data[ these ][['FINISH_LATITUDE','FINISH_LONGITUDE']].dropna().drop_duplicates().reset_index(drop=True)
     aux['idx'] = [ addresses.loc[ (lat,lon) ].idx for (lat,lon) in aux.values ]
-    orders = data[these & ((data.ACTION =='pickup') | (data.ACTION =='deliver')) ].fillna(0).copy().reset_index(drop=True)
+    orders = data[these & ((data.ACTION =='pickup') | (data.ACTION =='deliver')) ][['ACTION','ORDER','FINISH_LATITUDE','FINISH_LONGITUDE','EARLIEST_START_TIME','LATEST_START_TIME','LOAD_LM','LOAD_KG']].copy().reset_index(drop=True)
+    orders['LOAD_LM'] = orders.LOAD_LM.fillna(0)
+    orders['LOAD_KG'] = orders.LOAD_LM.fillna(0)
     orders['ORDER'] = orders.ORDER.astype(int)
     orders['LM'] = np.round( np.diff([0]+orders.LOAD_LM.to_list()).tolist(), nof_decimals )
     orders['KG'] = np.round( np.diff([0]+orders.LOAD_KG.to_list()).tolist(), nof_decimals )
